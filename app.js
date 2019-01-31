fe7382651063fee071f7028f23274e6a4309cdbf
Georgina Neilson <georgina.neilson@qnrl.com>
2019-01-31 16:49:16 +0000

    Add filters and params

--- app.js
+++ app.js
@@ -1,170 +1,44 @@
-let express = require('express');
-let fs = require('fs');
-let app = express();
-let dataFile = require('./flights.json');
-let bodyParser = require('body-parser');
-const querystring = require('querystring');
-
-// let getFlightById = 
-
-
-var http = require('http'),
-    url = require('url'),
-    query = url.parse(req.url,true).query;
-
-    app.get(query, (req, res, next) => {
-      // var query = url.parse(req.url,true).query;
-      res.end(JSON.stringify(query));
-      // fs.writeFileSync('departures.json', departuresFile);
-      // fs.readFile('departures.json', function(err, data){
-      //   console.log(data);
-      //   res.end(data);
-      // })
-    });
-
-// http.createServer(function (req, res) {
-    
-//     res.end(JSON.stringify(query));
-// }).listen(3333);
-// console.log("Server running at http://localhost:3333/");
-
-
-
-
-
-// let dataFile = "";
-
-//parse incoming requests data
-// app.use(bodyParser.urlencoded({extended: false}));
-// app.use(bodyParser.json());
-
-// fs.readFile('./users.json', 'utf8', (err, fileContents) => {
-//   if (err) {
-//     console.error(err)
-//     return
-//   }
-//   try {
-//     dataFile = JSON.parse(fileContents)
-//     console.log(dataFile);
-//   } catch (err) {
-//     console.error(err)
-//   }
-// });
-
-// let test = JSON.stringify(dataFile); 
-// fs.writeFileSync('test.json', test);  
-
-let arrivals = dataFile.filter(flight => flight.ArrDep == 'A');
-let arrivalsFile = JSON.stringify(arrivals);
-let departures = dataFile.filter(flight => flight.ArrDep == 'D');
-let departuresFile = JSON.stringify(departures);
-let flightnumber = dataFile.filter(flight => flight.FlightNo == flightNumber);
-// fs.writeFileSync('arrivals.json', arrivals);
+const express = require('express');
+const app = express();
+const dataFile = require('./flights.json');
 
 app.get("/flights", (req, res, next) => {
-  // res.end(dataFile);
-//   res.status(200).send({
-// test
-//   })
-
-  fs.readFile("flights.json", function(err, data){
-    console.log(data);
-    res.end(data);
-  })
+res.send(dataFile);
 });
 
 app.get("/flights/arrivals", (req, res, next) => {
-
-  fs.writeFileSync('arrivals.json', arrivalsFile);
-  fs.readFile('arrivals.json', function(err, data){
-    console.log(data);
-    res.end(data);
-  })
+  const arrivals = dataFile.filter(flight => flight.ArrDep == 'A');
+  res.send(arrivals);
 });
 
 app.get("/flights/departures", (req, res, next) => {
-
-  fs.writeFileSync('departures.json', departuresFile);
-  fs.readFile('departures.json', function(err, data){
-    console.log(data);
-    res.end(data);
-  })
+  const departures = dataFile.filter(flight => flight.ArrDep == 'D');
+  res.send(departures);
 });
 
-app.get("/flights/{flightNumber}", (req, res, next) => {
+app.get('/test/:userId', function (req, res) {
+  console.log(req.params);
+  let params = req.params.userId;
+  let id = params.userId;
+  console.log(id);
+  let idFound = dataFile.filter(flight => flight.FlightNo == id);
+  console.log(idFound);
+  res.send(idFound);
+})
 
-  fs.writeFileSync('departures.json', departuresFile);
-  fs.readFile('departures.json', function(err, data){
-    console.log(data);
-    res.end(data);
-  })
-});
+ // Handle 404
+ app.use(function (req, res) {
+   res.status(404).send('404: File not found');
+  //  res.render('404.jade', {
+  //    title: '404: File Not Found'
+  //  });
+ 
+ });
 
-// function searchByKey(arr, key){
-//   for(let counter = 0, length = arr.length; counter < length; counter++){
-//     // console.log("counter: ", counter);
-//     // console.log("length: ", length);
-//     console.log(arr);
-//     // if(arr[counter]['Key'] === key){
-//     // return arr[counter]['Values'];
-//     // }
-//   }
-//   return false;
-// }
-
-// let jsonfile = JSON.parse(dataFile);
-// let jsonstring = JSON.stringify(dataFile);
-
-// get all todos
-// app.get('/test', (req, res) => {
-//   res.status(200).send({
-//     success: 'true',
-//     message: 'todos retrieved successfully',
-//     todos: jsonstring
-//   })
-// });
-
-
-// app.get("/listNames", (req, res, next) =>{
-
-// });
-
-// app.get("/listNames", (req, res, next) => {
-  
-//  fs.readFile(dataFile, function(err, data){
-//    let test = JSON.parse(data);
-//    console.log(test);
-//   //  let test = searchByKey(data, "name");
-
-//   // //  console.log(test);
-//   //  console.log(err);   
-//   // //  console.log(d;
-//   //  res.end(test);
-//   //  console.log(data);
-//   console.log(test);
-//    res.end(test);
-//  })
-// });
-
-// var user = {
-//   "user4": {
-//     "name": "mohit",
-//     "password": "password4",
-//     "profession": "teacher",
-//     "id": 4
-//   }
-// }
-
-// app.post("/addUsers", (req, res, next) => {
-//   fs.readFile(dataFile, function (err, data) {
-//     data = JSON.parse(data);
-//     data["user4"] = user["user4"];
-    
-//     console.log(data);
-//     res.end(JSON.stringify(data));
-//   })
-// });
-
+ // Handle 500
+ app.use(function (error, req, res, next) {
+   res.status(500).send('Internal server error: ' + error);
+ });
 
 app.listen(3000, () => {
   console.log('Server running on port 3000')
